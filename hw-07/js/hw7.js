f21fd461337bdec6179823bba0865c490a264c30
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-25 23:11:38 +0200

    add hw7

---
+++ hw-07/js/hw7.js
@@ -0,0 +1,11 @@
+//Task #1
+
+const firstNumber = Number(prompt("Please, enter first number"));
+const secondNumber = Number(prompt("Please, enter second number"));
+const thirdNumber = Number(prompt("Please, enter third number"));
+
+const arithmeticalMean = (firstNumber + secondNumber + thirdNumber) / 3;
+
+alert(
+  `Arithmetical mean of ${firstNumber}, ${secondNumber}, ${thirdNumber} equal ${arithmeticalMean}`
+);
