7c19723886d55a77f5de9b699b75b60043f03f37
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-26 17:36:25 +0300

    add hw9

---
+++ hw-09/js/hw9.js
@@ -0,0 +1,23 @@
+let numOrStr = prompt("input number or string");
+console.log(numOrStr);
+
+if (numOrStr) {
+  numOrStr = numOrStr.trim();
+  if (numOrStr.length > 0 && isNaN(+numOrStr)) {
+    numOrStr = isNaN(+numOrStr);
+  }
+}
+
+switch (numOrStr) {
+  case null:
+    console.log("ви скасували");
+    break;
+  case "":
+    console.log("Empty String");
+    break;
+  case true:
+    console.log(" number is Ba_NaN");
+    break;
+  default:
+    console.log("OK!");
+}
