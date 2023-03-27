ee551a1a35bad2dbd5ba8a179261002d78a71efb
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-27 22:56:28 +0300

    add trim to operationType

--- hw-04/js/hw4.js
+++ hw-04/js/hw4.js
@@ -17,7 +17,9 @@ if (!inputOperand1 || !inputOperand2) {
   if (isNaN(inputOperand1Numbered) || isNaN(inputOperand2Numbered)) {
     alert("Both operands should be numbers, please, reload the page");
   } else {
-    let operationType = prompt("Please, enter operation type (only / * - +)");
+    let operationType = prompt(
+      "Please, enter operation type (only / * - +)"
+    ).trim();
     if (operationType === "/") {
       if (inputOperand2Numbered === 0) {
         alert("Division by 0 is not allowed");
