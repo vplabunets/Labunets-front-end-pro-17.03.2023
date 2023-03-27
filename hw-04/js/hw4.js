b4ab659a8a7bdab9ef26871a25cd7f8cd86dded7
Volodymyr Labunets <vplabunets@gmail.com>
2023-03-27 22:49:30 +0300

    update hw4

--- hw-04/js/hw4.js
+++ hw-04/js/hw4.js
@@ -1,40 +1,47 @@
 //Task #1
-let inputOperand1 = Number(prompt("Please, enter first operand"));
-let inputOperand2 = Number(prompt("Please, enter second operand"));
+const inputOperand1 = prompt("Please, enter first operand");
+const inputOperand2 = prompt("Please, enter second operand");
 
-if (isNaN(inputOperand1) && isNaN(inputOperand2)) {
-  alert("Both operands should be numbers");
-  inputOperand1 = Number(prompt("Please, enter first operand"));
-  inputOperand2 = Number(prompt("Please, enter second operand"));
-}
-
-let operationType = prompt("Please, enter operation type");
-
-if (
-  operationType !== "/" &&
-  operationType !== "*" &&
-  operationType !== "-" &&
-  operationType !== "+"
-) {
-  operationType = prompt("You can enter only / * - +");
+if (!inputOperand1 || !inputOperand2) {
+  alert("Both operands should be entered, please, reload the page");
 } else {
-  if (operationType === "/") {
-    if (inputOperand2 === 0) {
-      alert("Division by 0 is not allowed");
+  const inputOperand1Trimmed = inputOperand1.trim();
+  const inputOperand2Trimmed = inputOperand2.trim();
+  const inputOperand1Numbered =
+    inputOperand1Trimmed === "" ? NaN : Number(inputOperand1Trimmed);
+  const inputOperand2Numbered =
+    inputOperand2Trimmed === "" ? NaN : Number(inputOperand2Trimmed);
+
+  let result = 0;
+
+  if (isNaN(inputOperand1Numbered) || isNaN(inputOperand2Numbered)) {
+    alert("Both operands should be numbers, please, reload the page");
+  } else {
+    let operationType = prompt("Please, enter operation type (only / * - +)");
+    if (operationType === "/") {
+      if (inputOperand2Numbered === 0) {
+        alert("Division by 0 is not allowed");
+      }
+      result = inputOperand1Numbered / inputOperand2Numbered;
     }
-    const result = inputOperand1 / inputOperand2;
-    alert(`${inputOperand1} ${operationType} ${inputOperand2} = ${result}`);
-  }
-  if (operationType === "*") {
-    const result = inputOperand1 * inputOperand2;
-    alert(`${inputOperand1} ${operationType} ${inputOperand2} = ${result}`);
-  }
-  if (operationType === "-") {
-    const result = inputOperand1 - inputOperand2;
-    alert(`${inputOperand1} ${operationType} ${inputOperand2} = ${result}`);
-  }
-  if (operationType === "+") {
-    const result = inputOperand1 + inputOperand2;
-    alert(`${inputOperand1} ${operationType} ${inputOperand2} = ${result}`);
+    if (operationType === "*") {
+      result = inputOperand1Numbered * inputOperand2Numbered;
+    }
+    if (operationType === "-") {
+      result = inputOperand1Numbered - inputOperand2Numbered;
+    }
+    if (operationType === "+") {
+      result = inputOperand1Numbered + inputOperand2Numbered;
+    }
+    if (
+      operationType === "/" ||
+      operationType === "*" ||
+      operationType === "-" ||
+      operationType === "+"
+    ) {
+      alert(
+        `${inputOperand1Numbered} ${operationType} ${inputOperand2Numbered} = ${result}`
+      );
+    } else alert("Operation type is invalid, please, reload the page");
   }
 }
