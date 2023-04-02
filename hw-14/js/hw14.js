54a92115652188ddf388e6decdd64751ad875d59
Volodymyr Labunets <vplabunets@gmail.com>
2023-04-02 20:08:12 +0300

    add hw14

---
+++ hw-14/js/hw14.js
@@ -0,0 +1,86 @@
+//Дан масив об'єктів.
+//Вивести масив телефонних номерів користувачів, у яких баланс більше 2000 доларів.
+//І знайти суму всіх балансів користувачів
+
+const users = [
+  {
+    index: 0,
+    isActive: true,
+    balance: "$2,226.60",
+    name: "Eugenia Sawyer",
+    gender: "female",
+    phone: "+1 (840) 583-3207",
+    address: "949 John Street, Rose, Puerto Rico, 1857",
+  },
+  {
+    index: 1,
+    isActive: true,
+    balance: "$2,613.77",
+    name: "Pauline Gallegos",
+    gender: "female",
+    phone: "+1 (985) 593-3328",
+    address: "328 Greenpoint Avenue, Torboy, North Dakota, 6857",
+  },
+  {
+    index: 2,
+    isActive: false,
+    balance: "$3,976.41",
+    name: "Middleton Chaney",
+    gender: "male",
+    phone: "+1 (995) 591-2478",
+    address: "807 Fleet Walk, Brutus, Arkansas, 9783",
+  },
+  {
+    index: 3,
+    isActive: true,
+    balance: "$1,934.58",
+    name: "Burns Poole",
+    gender: "male",
+    phone: "+1 (885) 559-3422",
+    address: "730 Seba Avenue, Osage, Alabama, 6290",
+  },
+  {
+    index: 4,
+    isActive: true,
+    balance: "$3,261.65",
+    name: "Mcfadden Horne",
+    gender: "male",
+    phone: "+1 (942) 565-3988",
+    address: "120 Scholes Street, Kirk, Michigan, 1018",
+  },
+  {
+    index: 5,
+    isActive: false,
+    balance: "$1,790.56",
+    name: "Suzette Lewis",
+    gender: "female",
+    phone: "+1 (837) 586-3283",
+    address: "314 Dunne Place, Bawcomville, Guam, 9053",
+  },
+];
+//  Вивести масив телефонних номерів користувачів, у яких баланс більше 2000 доларів.
+const minBalanceValue = 2000;
+const filteredUsers = users.filter(
+  (user) => user.balance.replace("$", "").replace(",", "") > minBalanceValue
+);
+console.log(
+  `Users array, whose balance is more than ${minBalanceValue} USD`,
+  filteredUsers
+);
+const phonesArray = filteredUsers.map((user) => user.phone);
+console.log(
+  `Phones array of users, whose balance is more than ${minBalanceValue} USD`,
+  phonesArray
+);
+//  І знайти суму всіх балансів користувачів
+const balancesArray = users.map((user) =>
+  parseFloat(user.balance.replace("$", "").replace(",", ""), 2)
+);
+const balancesSum = balancesArray.reduce((previousValue, balance) => {
+  return previousValue + balance;
+}, 0);
+const balancesSumEnIn = new Intl.NumberFormat("en-IN", {
+  style: "currency",
+  currency: "USD",
+}).format(balancesSum.toFixed(2));
+console.log("Balance sum of all users is equal", balancesSumEnIn);
