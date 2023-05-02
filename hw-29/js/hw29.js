a5ea6ee906fe3a994c6330008d06d953c922f5b4
Volodymyr Labunets <vplabunets@gmail.com>
2023-05-02 22:29:20 +0300

    update hw 29

--- hw-29/js/hw29.js
+++ hw-29/js/hw29.js
@@ -1,9 +1,9 @@
 const data = [
-  { index: 1, url: "&#128528", votes: 0 },
-  { index: 2, url: "&#128522", votes: 0 },
-  { index: 3, url: "&#129313", votes: 0 },
-  { index: 4, url: "&#128542", votes: 0 },
-  { index: 5, url: "&#128544", votes: 0 },
+  { id: 1, url: "&#128528", votes: 0 },
+  { id: 2, url: "&#128522", votes: 0 },
+  { id: 3, url: "&#129313", votes: 0 },
+  { id: 4, url: "&#128542", votes: 10 },
+  { id: 5, url: "&#128544", votes: 0 },
 ];
 
 const wrapperEl = document.querySelector(".wrapper");
@@ -13,24 +13,20 @@ const smilesListItemEl = document.querySelector(".smilesListItem");
 let markup = data
   .map(
     (element) =>
-      `<li class="smilesListItem"><div class="smile" data-index=${element.index}>${element.url}</div><div class="votes">${element.votes}</d></li>`
+      `<li class="smilesListItem"><div class="smile" data-id=${element.id}>${element.url}</div><div class="votes">${element.votes}</d></li>`
   )
   .join("");
 
 smilesListEl.innerHTML = markup;
-const votesEl = document.querySelector(".votes");
+const votesEl = document.querySelectorAll(".votes");
+console.log(votesEl);
 smilesListEl.addEventListener("click", onClickHandler);
 
 function onClickHandler(evt) {
-  const ind = evt.target.dataset.index;
-  const targetedEl = data.find((element) => element.index === Number(ind));
+  const ind = evt.target.dataset.id;
+  const targetedEl = data.find((element) => element.id === Number(ind));
   data[data.indexOf(targetedEl)].votes =
     data[data.indexOf(targetedEl)].votes + 1;
-  markup = data
-    .map(
-      (element) =>
-        `<li class="smilesListItem"><div class="smile" data-index=${element.index}>${element.url}</div><div class="votes">${element.votes}</d></li>`
-    )
-    .join("");
-  smilesListEl.innerHTML = markup;
+  votesEl[data.indexOf(targetedEl)].innerHTML =
+    data[data.indexOf(targetedEl)].votes;
 }
