a2d9ded04d566e092cdb1727c17163ac6875dd7a
Volodymyr Labunets <vplabunets@gmail.com>
2023-06-26 13:59:39 +0300

    fixed version 2

--- hw-62/js/index.js
+++ hw-62/js/index.js
@@ -1,122 +1,118 @@
-function Student(firstName, lastName, birthYear) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthYear = birthYear;
-  this.presence = new Array(10);
-  this.marks = new Array(10);
-  this.currentLesson = 0;
-  this.currentMark = 0;
+(function () {
+  function Student(firstName, lastName, birthYear, lessonsCount = 10) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.lessonsCount = lessonsCount;
+    this.presence = new Array(this.lessonsCount);
+    this.marks = new Array(this.lessonsCount);
+    this.currentLesson = 1;
 
-  Student.prototype.present = function () {
-    if (this.currentLesson < 10) {
-      this.presence[this.currentLesson] = true;
-      this.currentLesson += 1;
-    }
-  };
-  Student.prototype.absent = function () {
-    if (this.currentLesson < 10) {
-      this.presence[this.currentLesson] = false;
-      this.currentLesson += 1;
-    } else console.log("presence array is full");
-  };
-  Student.prototype.mark = function (mark) {
-    if (this.currentMark < 10) {
-      this.marks[this.currentMark] = mark;
-      this.currentMark += 1;
-    } else console.log("Marks array is full");
-  };
+    Student.prototype.setPresence = function (present, nextLesson = true) {
+      if (this.lessonsCount === this.currentLesson) return;
+      this.presence[this.currentLesson - 1] = present;
 
-  Student.prototype.getAge = function () {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const studentAge = year - this.birthYear;
-    return `${this.firstName} ${this.lastName} is ${studentAge}`;
-  };
-  Student.prototype.getAverageRate = function () {
-    let averageMark = 0;
-    if (this.mark.length !== 0) {
-      averageMark =
-        this.marks.reduce((sum, el) => sum + Number(el), 0) / this.marks.length;
-    }
-    return averageMark;
-  };
+      nextLesson ? (this.currentLesson += 1) : null;
+    };
 
-  Student.prototype.summary = function () {
-    const presenceDays = this.presence.filter((el) => el === true).length;
-    const totalDays = this.presence.length;
-    const averagePresence = +(presenceDays / totalDays).toFixed(1);
-    const averageMark = +this.getAverageRate();
+    Student.prototype.present = function (mark = null) {
+      if (mark !== null && !this.validateMark(mark)) return;
+      this.setPresence(true, mark === null);
+      if (mark !== null) {
+        this.setMark(mark);
+        this.currentLesson += 1;
+      }
+    };
+    Student.prototype.absent = function () {
+      this.setPresence(false);
+    };
+    Student.prototype.setMark = function (mark) {
+      if (!this.validateMark(mark)) return;
+      this.marks[this.currentLesson - 1] = mark;
+    };
+    Student.prototype.validateMark = function (mark) {
+      if (mark >= 0 && mark <= 10) return true;
+      console.warn("Mark is not correct");
+      return false;
+    };
 
-    if (averagePresence > 0.9 && averageMark > 9) {
-      return "Ути какой молодчинка!";
-    }
-    if (
-      (averagePresence <= 0.9 && averageMark > 9) ||
-      (averagePresence > 0.9 && averageMark <= 9)
-    ) {
-      return "Норм, но можно лучше";
-    } else {
-      return "Редиска!";
-    }
-  };
-}
+    Student.prototype.getAge = function () {
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const studentAge = year - this.birthYear;
+      return `${this.firstName} ${this.lastName} is ${studentAge}`;
+    };
 
-const Dua = new Student("Dua", "Lipa", 1995);
-Dua.absent();
-Dua.present();
-Dua.present();
-Dua.present();
-Dua.present();
-Dua.present();
-Dua.present();
-Dua.present();
-Dua.present();
-Dua.present();
-Dua.mark(10);
-Dua.mark(9);
-Dua.mark(10);
-Dua.mark(9);
-Dua.mark(10);
-Dua.mark(9);
-Dua.mark(10);
-Dua.mark(9);
-Dua.mark(10);
-Dua.mark(9);
-Dua.mark(10);
-Dua.mark(9);
+    Student.prototype.getPresenceDays = function () {
+      return this.presence.filter((el) => el === true).length;
+    };
 
-console.log(Dua.getAge());
-console.log(
-  `Average mark of ${Dua.firstName} ${Dua.lastName} is ${Dua.getAverageRate()}`
-);
-console.log(Dua.summary());
+    Student.prototype.getAveragePresence = function () {
+      if (this.currentLesson > 0) {
+        const averagePresence = (
+          this.getPresenceDays() /
+          (this.currentLesson - 1)
+        ).toFixed(1);
+        return averagePresence;
+      }
+      return 1;
+    };
 
-const Volodymyr = new Student("Volodymyr", "Samoluk", 1994);
-Volodymyr.absent();
-Volodymyr.present();
-Volodymyr.absent();
-Volodymyr.present();
-Volodymyr.present();
-Volodymyr.absent();
-Volodymyr.present();
-Volodymyr.present();
-Volodymyr.present();
-Volodymyr.absent();
-Volodymyr.mark(1);
-Volodymyr.mark(9);
-Volodymyr.mark(10);
-Volodymyr.mark(9);
-Volodymyr.mark(10);
-Volodymyr.mark(9);
-Volodymyr.mark(10);
-Volodymyr.mark(9);
-Volodymyr.mark(2);
-Volodymyr.mark(9);
+    Student.prototype.getAverageRate = function () {
+      let marksSum = 0;
+      let marksQuantity = 0;
+      if (this.marks.length !== 0) {
+        marksSum = this.marks.reduce((sum, el) => {
+          if (typeof el === "number") {
+            sum += el;
+            marksQuantity += 1;
+          }
+          return sum;
+        }, 0);
+      }
+      if (!marksQuantity) return 0;
+      return (marksSum / marksQuantity).toFixed(1);
+    };
 
-console.log(Volodymyr.getAge());
-console.log(
-  `Average mark of ${Volodymyr.firstName} ${
-    Volodymyr.lastName
-  } is ${Dua.getAverageRate()}`
-);
-console.log(Volodymyr.summary());
+    Student.prototype.summary = function () {
+      const averagePresence = this.getAveragePresence();
+      const averageMark = this.getAverageRate();
+      if (averagePresence > 0.9 && averageMark > 9) {
+        return "Ути какой молодчинка!";
+      }
+      if (
+        (averagePresence <= 0.9 && averageMark > 9) ||
+        (averagePresence > 0.9 && averageMark <= 9)
+      ) {
+        return "Норм, но можно лучше";
+      } else {
+        return "Редиска!";
+      }
+    };
+  }
+
+  const Volodymyr = new Student("Volodymyr", "Samoluk", 1994);
+  Volodymyr.absent();
+  Volodymyr.present(10);
+  Volodymyr.absent();
+  Volodymyr.present(10);
+  Volodymyr.present(10);
+  Volodymyr.absent();
+  Volodymyr.present(10);
+  Volodymyr.present(10);
+  Volodymyr.present(10);
+  Volodymyr.absent();
+
+  console.log(Volodymyr.getAge());
+  console.log(
+    `Average presence of ${Volodymyr.firstName} ${
+      Volodymyr.lastName
+    } is ${Volodymyr.getAveragePresence()}`
+  );
+  console.log(
+    `Average mark of ${Volodymyr.firstName} ${
+      Volodymyr.lastName
+    } is ${Volodymyr.getAverageRate()}`
+  );
+  console.log(Volodymyr.summary());
+})();
