7c5ce448ed04e6b2ac7fb688a13176c5d420d529
Volodymyr Labunets <vplabunets@gmail.com>
2023-06-29 23:55:38 +0300

    add hw-66

---
+++ hw-66/js/script.js
@@ -0,0 +1,12 @@
+const goods = [['футболка', 15], ['шорты', 25], ['носки', 5]];
+
+console.log((function sum  () {
+  let goodsSum = 0
+  const goodsMapped = new Map(goods)
+  goodsMapped.forEach((value,) => goodsSum += value)
+
+  return goodsSum})())
+
+
+
+
