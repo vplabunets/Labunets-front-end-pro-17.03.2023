7ead40d8b75404fd5654b9b6c1c162820bbcbe33
Volodymyr Labunets <vplabunets@gmail.com>
2023-06-29 23:20:41 +0300

    finish hw-25

--- hw-25/js/script.js
+++ hw-25/js/script.js
@@ -1,48 +1,65 @@
 //  indexOf;
-function indexOf(array, element) {
-  for (let i = 0; i <= array.length; i += 1) {
+const arr1 = ["a", "b", "c"];
+function indexOf(array, element, fromIndex = 0) {
+  for (let i = fromIndex; i <= array.length; i += 1) {
     if (array[i] === element) return i;
   }
   return -1;
 }
+console.log(indexOf(arr1, "a"));
 
-console.log(indexOf([1, 2, 3], 55));
-
-function lastIndexOf(array, element) {
+//lastIndexOf
+const arr2 = [1, 2, 3, 1, 2, 3, 1, 2, 3];
+function lastIndexOf(array, element, startIndex = array.length) {
   let lastIndex = -1;
-  for (let i = 0; i <= array.length; i += 1) {
-    if (array[i] === element) {
-      lastIndex = i;
-    }
+  for (let i = 0; i <= array.length + startIndex; i += 1) {
+    if (array[i] === element) lastIndex = i;
   }
   return lastIndex;
 }
+console.log(lastIndexOf(arr2, 3, -2));
 
-console.log(lastIndexOf([1, 2, 3, 1, 2, 3, 1, 2, 3], 3));
-
-function find(array, element) {
-  for (let i = 0; i <= array.length; i += 1) {
-    if (array[i] === element) {
-      return array[i];
-    }
+//find
+function find(array, callback) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (callback(array[index], index, array)) return array[index];
   }
   return undefined;
 }
+console.log(find(arr2, (element, index, array) => element === 2));
 
-console.log(find([1, 2, 3, 1, 2, 3, 1, 2, 3], 33));
-
-function findIndex(array, element) {
-  for (let i = 0; i <= array.length; i += 1) {
-    if (array[i] === element) {
-      return i;
-    }
+// findIndex
+function findIndex(array, callback) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (callback(array[index], index, array)) return index;
   }
   return -1;
 }
+console.log(findIndex(arr2, (element, index, array) => element === 11));
 
-console.log(find([1, 2, 3, 1, 2, 3, 1, 2, 3], 33));
+//includes
+function includes(array, element, fromIndex = 0) {
+  for (let i = fromIndex; i <= array.length; i += 1) {
+    if (array[i] === element) return true;
+  }
+  return false;
+}
+console.log(includes(arr1, "c"));
 
-const xxx = [[1, 2, 3], { a: 1, b: 2 }, 1, 2, 3, 1, 2, 3].find(
-  (el, index, array) => el.a === 1
-);
-console.log(xxx);
+//every
+function every(array, callback) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (!callback(array[index], index, array)) return false;
+  }
+  return true;
+}
+console.log(every(arr2, (element, index, array) => element >= 1));
+
+//some
+function some(array, callback) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (callback(array[index], index, array)) return true;
+  }
+  return false;
+}
+console.log(some(arr2, (element, index, array) => element >= 10));
