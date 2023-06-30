47f964cbc0195f3208989d016ea691b502eaad44
Volodymyr Labunets <vplabunets@gmail.com>
2023-06-30 10:28:13 +0300

    add hw-70

--- hw-67/js/script.js
+++ hw-67/js/script.js
@@ -1,11 +1,13 @@
-const string = "abracadabra";
+const students = [
+    { name: 'John', age: 20, subject: 'Math' },
+    { name: 'Alice', age: 22, subject: 'English' },
+    { name: 'Bob', age: 21, subject: 'History' }
+];
 
 (function (){
-      function findUniqueElementsQuantity(string){
-        const elementsArray = new Set(string.split("") )
-        return elementsArray.size
+      function printGreetings(data){
+         data.forEach(({name, subject})=> console.log(`Привет, ${name}! Ты изучаешь предмет ${subject}`))
       }
-
-      console.log(`String contains ${findUniqueElementsQuantity(string)} unique elements`);
-})()
+      printGreetings(students)
+ })()
 
