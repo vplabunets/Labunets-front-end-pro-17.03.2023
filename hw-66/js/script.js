c1e11777a5cfb85b5f7b201a660eb973b19636e8
Volodymyr Labunets <91912993+vplabunets@users.noreply.github.com>
2023-06-30 08:40:32 +0300

    Update script.js

--- hw-66/js/script.js
+++ hw-66/js/script.js
@@ -3,7 +3,7 @@ const goods = [['футболка', 15], ['шорты', 25], ['носки', 5]];
 console.log((function sum  () {
   let goodsSum = 0
   const goodsMapped = new Map(goods)
-  goodsMapped.forEach((value,) => goodsSum += value)
+  goodsMapped.forEach((value) => goodsSum += value)
 
   return goodsSum})())
 
