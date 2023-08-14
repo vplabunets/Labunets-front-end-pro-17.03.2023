2de5663f6853decaed8ba17065c8918a26ac3ffc
Volodymyr Labunets <vplabunets@gmail.com>
2023-08-14 22:54:35 +0300

    add hw-75

---
+++ hw-75/src/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Card() {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some quick example text to build on the card</p>
+                <button type="button" className="btn btn-primary">Go somewhere</button>
+            </div>
+        </div>
+    )
+}
+
+
+
+ 
