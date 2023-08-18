8bf24a6c3f25c1a0eca0e463700ef8641dd5d589
Volodymyr Labunets <vplabunets@gmail.com>
2023-08-18 11:58:48 +0300

    add gitignore

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Definitions from "../Definitions/Definitions";
+import { definitions } from "../../data/data";
+
+function App() {
+  return (
+    <div className="container p-5">
+      <Definitions data={definitions} />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
